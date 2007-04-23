807ac14fab22fe196004e6b4d5beb0b8a89f4a3a
rberdeen <rberdeen@36d3eade-8fda-4651-a852-104dea8b868f>
2007-04-23 06:12:47 +0000

    added property list, which makes building a json object from a pojo object graph much easier
    git-svn-id: http://svn.ry1.org/sandbox/json@133 36d3eade-8fda-4651-a852-104dea8b868f

--- src/java/org/ry1/json/JsonObject.java
+++ src/java/org/ry1/json/JsonObject.java
@@ -4,7 +4,10 @@ import java.beans.BeanInfo;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+
+import org.ry1.json.PropertyList.PropertyListElement;
 
 public class JsonObject implements Json {
 	private LinkedHashMap<String, Object> map = new LinkedHashMap<String, Object>();
@@ -31,6 +34,49 @@ public class JsonObject implements Json {
 			for (String propertyName : propertyNames) {
 				Method getter = beanClass.getMethod("get" + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1), (Class[]) null);
 				set(propertyName, getter.invoke(bean, (Object[]) null));
+			}
+		}
+		catch (Exception ex) {
+			throw new IllegalArgumentException("Bean cannot be introspected", ex);
+		}
+	}
+	
+	public JsonObject(Object bean, PropertyList propertyList) {
+		try {
+			Class beanClass = bean.getClass();
+			for (PropertyListElement propertyListElement : propertyList.getElements()) {
+				String propertyName = propertyListElement.propertyName;
+				Method getter = beanClass.getMethod("get" + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1), (Class[]) null);
+				Object property = getter.invoke(bean, (Object[]) null);
+				
+				Object value = property;
+				
+				if (property != null) {
+					if (propertyListElement.list) {
+						// list property
+						JsonArray array = new JsonArray();
+						Iterable propertyIterable = property.getClass().isArray() ? Arrays.asList((Object[]) property) : (Iterable) property;
+						
+						if (propertyListElement.propertyList != null) {
+							for (Object element : propertyIterable) {
+								array.addValue(new JsonObject(element, propertyListElement.propertyList));
+							}
+						}
+						else {
+							for (Object element : propertyIterable) {
+								array.addValue(element);
+							}
+						}
+						
+						value = array;
+					}
+					else if (propertyListElement.propertyList != null) {
+						// bean property
+						value = new JsonObject(property, propertyListElement.propertyList);
+					}
+				}
+
+				set(propertyName, value);
 			}
 		}
 		catch (Exception ex) {
---
+++ src/java/org/ry1/json/PropertyList.java
@@ -0,0 +1,86 @@
+/* $Id$ */
+
+package org.ry1.json;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/** Stores the heirarchy of bean properties to use to build a json object.
+ * @author Ryan Berdeen
+ *
+ */
+public class PropertyList {
+	private ArrayList<PropertyListElement> elements = new ArrayList<PropertyListElement>();
+	
+	/** Adds the named value to the list of properties.
+	 */
+	public PropertyList includeValue(String propertyName) {
+		return includeBean(propertyName, null);
+	}
+	
+	/** Adds the named value to the list of properties.
+	 */
+	public PropertyList includeValues(String... propertyNames) {
+		for (String propertyName : propertyNames) {
+			includeValue(propertyName);
+		}
+		
+		return this;
+	}
+	
+	/** Adds the named bean and the bean's properties to the list of properties.
+	 */
+	public PropertyList includeBean(String propertyName, PropertyList propertyList) {
+		elements.add(new PropertyListElement(propertyName, propertyList, false));
+		return this;
+	}
+	
+	/** Adds the named bean to the list of properties and return the <code>PropertyList</code> for the bean.
+	 */
+	public PropertyList forBean(String propertyName) {
+		PropertyList propertyList = new PropertyList();
+		includeBean(propertyName, propertyList);
+		return propertyList;
+	}
+	
+	/** Adds the named list of values to the list of properties.
+	 */
+	public PropertyList includeListOfValues(String propertyName) {
+		return includeListOfBeans(propertyName, null);
+	}
+	
+	/** Adds the named list of beans and the beans' properties to the list of properties.
+	 * @param propertyName
+	 * @param propertyList
+	 * @return
+	 */
+	public PropertyList includeListOfBeans(String propertyName, PropertyList propertyList) {
+		elements.add(new PropertyListElement(propertyName, propertyList, true));
+		return this;
+	}
+	
+	/** Adds the named list of beans to the property list and returns the <code>PropertyList</code> for the beans.
+	 */
+	public PropertyList forListOfBeans(String propertyName) {
+		PropertyList propertyList = new PropertyList();
+		includeListOfBeans(propertyName, propertyList);
+		return propertyList;
+	}
+	
+	List<PropertyListElement> getElements() {
+		return Collections.unmodifiableList(elements);
+	}
+	
+	static class PropertyListElement {
+		public final boolean list;
+		public final String propertyName;
+		public final PropertyList propertyList;
+		
+		public PropertyListElement(String propertyName, PropertyList propertyList, boolean list) {
+			this.propertyName = propertyName;
+			this.propertyList = propertyList;
+			this.list = list;
+		}
+	}
+}
